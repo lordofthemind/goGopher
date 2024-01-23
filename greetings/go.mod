e4cf3c87722e199c83bb1fae3acd6f71f1388add
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-01-23 21:28:12 +0000

    module creation

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module manishh.me/greetings
+
+go 1.21.5
---
+++ hello/main.go
@@ -0,0 +1,101 @@
+// package main
+
+// import (
+//     "fmt"
+
+//     "manishh.me/greetings"
+// )
+
+// func main() {
+//     // Get a greeting message and print it.
+//     message := greetings.Hello("Manishh")
+//     fmt.Println(message)
+// }
+
+// package main
+
+// import (
+//     "fmt"
+//     "log"
+
+//     "manishh.me/greetings"
+// )
+
+// func main() {
+//     // Set properties of the predefined Logger, including
+//     // the log entry prefix and a flag to disable printing
+//     // the time, source file, and line number.
+//     log.SetPrefix("greetings: ")
+//     log.SetFlags(0)
+
+//     // Request a greeting message.
+//     message, err := greetings.Hello("")
+//     // If an error was returned, print it to the console and
+//     // exit the program.
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+
+//     // If no error was returned, print the returned message
+//     // to the console.
+//     fmt.Println(message)
+// }
+
+// package main
+
+// import (
+//     "fmt"
+//     "log"
+
+//     "manishh.me/greetings"
+// )
+
+// func main() {
+//     // Set properties of the predefined Logger, including
+//     // the log entry prefix and a flag to disable printing
+//     // the time, source file, and line number.
+//     log.SetPrefix("greetings: ")
+//     log.SetFlags(0)
+
+//     // Request a greeting message.
+//     message, err := greetings.Hello("Manish")
+//     // If an error was returned, print it to the console and
+//     // exit the program.
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+
+//     // If no error was returned, print the returned message
+//     // to the console.
+//     fmt.Println(message)
+// }
+
+
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "manishh.me/greetings"
+)
+
+func main() {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Manish"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
+}
